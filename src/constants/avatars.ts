df47a46abd7b2f16b860c95fe2c4b02db355f426
Edwin Anel González <aneh6272@gmail.com>
2024-08-07 06:43:45 -0500

    options of avatars

---
+++ src/constants/avatars.ts
@@ -0,0 +1,11 @@
+import amyAvatar from '../assets/images/avatars/image-amyrobson.webp'
+import juliusAvatar from '../assets/images/avatars/image-juliusomo.webp'
+import maxAvatar from '../assets/images/avatars/image-maxblagun.webp'
+import ramsesAvatar from '../assets/images/avatars/image-ramsesmiron.webp'
+
+export const avatars = [
+  { src: amyAvatar, alt: 'Amy Avatar' },
+  { src: juliusAvatar, alt: 'Julius Avatar' },
+  { src: maxAvatar, alt: 'Max Avatar' },
+  { src: ramsesAvatar, alt: 'Ramses Avatar' }
+]
